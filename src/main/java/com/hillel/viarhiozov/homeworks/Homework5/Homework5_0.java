81a659ef641f3e41926975a25beaa48f25cb2bf3
Muda Muda <mr,acksel@gmail.com>
2022-11-06 21:28:01 +0300

    v1.1 working solution for any ship count inputted by user

--- src/main/java/com/hillel/viarhiozov/homeworks/Homework5/Homework5_0.java
+++ src/main/java/com/hillel/viarhiozov/homeworks/Homework5/Homework5_0.java
@@ -1,5 +1,7 @@
 package com.hillel.viarhiozov.homeworks.Homework5;
 
+import java.util.Scanner;
+
 public class Homework5_0 {
 
     public static void main(String[] args) {
@@ -8,23 +10,54 @@ public class Homework5_0 {
 //        У Японії числа 4 та 9 вважаються нещасливими. Вам потрібно пронумерувати 100 космічних
 //        шатлів для перевезення людей на Марс так, щоб у номерах шатлів не траплялося нещасливих чисел.
 //        Напишіть функцію, яка виводить усі номери таких шатлів.
+        Scanner scanner = new Scanner(System.in);
 
+        int shipCount = 0;
+
+        while (true) {
+            System.out.println("\nEnter Ship Count");
+            if (scanner.hasNextInt()) {
+                shipCount = scanner.nextInt();
+                break;
+            } else {
+                System.out.println("\nWrong input! Only int allowed!");
+                scanner.nextLine();
+            }
+        }
 
         int luckyShuttleCount = 0;
 
 
-        for (int i = 1; luckyShuttleCount < 100; i++) {
+        for (int i = 1; luckyShuttleCount < shipCount; i++) {
 
-            if (i % 10 != 4 && i % 10 != 9 && (i / 10) % 10 != 4 && (i / 10) % 10 != 9) {
+            int ship = i;
 
-                System.out.print(i + " ");
+            while (ship != 0) {
 
-                luckyShuttleCount++;
+
+                if (ship % 10 == 4 || ship % 10 == 9) {
+
+                    break;
+
+                }
+                ship /= 10;
 
             }
 
-        }
+            if (ship == 0) {
 
+
+                System.out.print(i + " ");
+                luckyShuttleCount++;
+                if (luckyShuttleCount % 20 == 0) {
+                    System.out.println();
+                }
+
+
+            }
+
+
+        }
 
     }
 
