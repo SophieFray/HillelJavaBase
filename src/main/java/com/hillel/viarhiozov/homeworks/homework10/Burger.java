29c699e7e833f26ee1090a296a330ab32302a7d8
Alex Volkov <mr.acksel@gmail.com>
2022-12-06 15:13:44 +0200

    homework done

---
+++ src/main/java/com/hillel/viarhiozov/homeworks/homework10/Burger.java
@@ -0,0 +1,47 @@
+package com.hillel.viarhiozov.homeworks.homework10;
+
+public class Burger {
+
+    String bun;
+    String meat;
+    boolean isDoubleMeat = false;
+    String salad;
+    String cheese;
+    String mayonnaise;
+
+    public Burger(String bun, String meat, String salad, String cheese, String mayonnaise) {
+
+        this.bun = bun;
+        this.meat = meat;
+        this.salad = salad;
+        this.cheese = cheese;
+        this.mayonnaise = mayonnaise;
+        System.out.println();
+        System.out.println("Standard composition: " + "\nMeat: " + meat + "\nBun: " + bun + "\nSalad: " + salad + "\nMayonnaise: " + mayonnaise + "\nCheese: " + cheese);
+
+    }
+
+    public Burger(String bun, String meat, String salad, String cheese) {
+        this.bun = bun;
+        this.meat = meat;
+        this.salad = salad;
+        this.cheese = cheese;
+        System.out.println();
+        System.out.println("Diet Burger composition: " + "\nMeat: " + meat + "\nBun: " + bun + "\nSalad: " + salad + "\nMayonnaise: " + "\nCheese: " + cheese);
+
+    }
+
+    public Burger(String bun, String meat, String salad, String cheese, String mayonnaise, boolean isDoubleMeat) {
+
+        this.meat = meat;
+        this.bun = bun;
+        this.salad = salad;
+        this.cheese = cheese;
+        this.mayonnaise = mayonnaise;
+        this.isDoubleMeat = true;
+        System.out.println();
+        System.out.println("Double Meat composition: " + "\nDouble meat: " + this.meat + "\nBun: " + this.bun + "\nSalad: " + salad + "\nMayonnaise: " + this.mayonnaise + "\nCheese: " + cheese);
+    }
+
+
+}
